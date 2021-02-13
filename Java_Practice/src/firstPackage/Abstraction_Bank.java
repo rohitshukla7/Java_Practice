042934d08fa1f99dfc3784ae0a618c2ba647bc7b
Rohit Shukla <zrohit95@gmail.com>
2021-02-13 21:09:26 +0530

    first commit via eclipse

---
+++ Java_Practice/src/firstPackage/Abstraction_Bank.java
@@ -0,0 +1,30 @@
+package firstPackage;
+
+
+public class Abstraction_Bank {
+
+	public static void main(String[] args) {
+		Bank s = new SBI();
+		System.out.println("SBI Interest:"+s.getBankInterest());
+		Bank h = new HDFC();
+		System.out.println(h.getBankInterest());
+	}
+
+}
+
+abstract class Bank{
+	abstract int getBankInterest();
+}
+
+class SBI extends Bank{
+	int getBankInterest() {
+		return 7;
+	}
+}
+
+class HDFC extends Bank{
+	int getBankInterest() {
+		System.out.print("HDFC Interest:");
+		return 8;
+	}
+}
