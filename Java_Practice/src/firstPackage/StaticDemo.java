7b8917cb6a29c4d7175fad35bbe8a697f1636387
Rohit Shukla <zrohit95@gmail.com>
2021-02-20 21:17:41 +0530

    commit static class

--- Java_Practice/src/firstPackage/StaticDemo.java
+++ Java_Practice/src/firstPackage/StaticDemo.java
@@ -4,6 +4,7 @@ public class StaticDemo {
 	
 	static int empid;
 	static String empname;
+	private static double salary = 20000;
 	
 	static {
 		empid = 123;
@@ -11,12 +12,21 @@ public class StaticDemo {
 		System.out.println("in static block");
 		//System.exit(0);
 	}
+	
+	static class innerClass{
+		void salary() {
+			System.out.println("Salary of employee "+empname+" is "+salary);
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Employee ID:"+empid);
 		System.out.println("Employee Name:"+empname);
 		
 		show(); //calling static method
+		
+		StaticDemo.innerClass obj = new StaticDemo.innerClass();
+		obj.salary();
 
 	}
 	
